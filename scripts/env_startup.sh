8e91c170af891722b6d952a643599f64ab930c0d
Bartłomiej Konieczny <bartlomiej.konieczny@sabre.com>
2016-10-26 15:10:33 +0200

    update

--- scripts/env_startup.sh
+++ scripts/env_startup.sh
@@ -1,13 +1,22 @@
 #! /bin/bash
+
 firefox &
-wait
-/home/redi/idea-IU-162.2032.8/bin &
-wait
-xfce4-terminal &
-wait
-code &
-wait
+sleep 3
 wmctrl -r firefox -t 0
-wmctrl -r idea -t 1
-wmctrl -r xfce4-terminal -t 2
+
+code &
+sleep 2
 wmctrl -r code -t 3
+
+xfce4-terminal &
+sleep 2
+wmctrl -r terminal -t 2
+
+
+/home/redi/idea-IU-162.2032.8/bin/idea.sh &
+sleep 20
+wmctrl -r idea -t 1
+
+
+
+
